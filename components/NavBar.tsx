aeceff638dd7c95cc6cb5479933ef4cde51829b4
Thomas <totoalexandre6259@gmail.com>
2024-06-13 03:05:05 +0200

    NavBar ok

---
+++ components/NavBar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+import Link from "next/link";
+
+export const path = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Article",
+    href: "/dashboard/article",
+  },
+  {
+    name: "Category",
+    href: "/dashboard/category",
+  },
+  {
+    name: "Edit",
+    href: "/dashboard/category/edit",
+  },
+];
+
+export const NavBar = () => {
+  return (
+    <header className="w-full h-14 flex items-center justify-center py-4">
+      <NavigationMenu>
+        <NavigationMenuList className="w-full flex flex-row gap-[30px]">
+          {path.map((item) => (
+            <NavigationMenuItem key={item.name}>
+              <Link href={item.href} legacyBehavior passHref>
+                <NavigationMenuLink>{item.name}</NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
