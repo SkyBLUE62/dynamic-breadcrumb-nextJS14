d20ff97b60c8bb406517092b6711dca5e7455aab
Thomas <totoalexandre6259@gmail.com>
2024-06-13 03:05:29 +0200

    init demo page

--- app/dashboard/category/page.tsx
+++ app/dashboard/category/page.tsx
@@ -1,6 +1,6 @@
 import { DynamicBreadcrumb } from "@/components/DynamicBreadcrumb";
 
-export default function Home() {
+export default function CategoryPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <DynamicBreadcrumb />
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { DynamicBreadcrumb } from "@/components/DynamicBreadcrumb";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <DynamicBreadcrumb />
